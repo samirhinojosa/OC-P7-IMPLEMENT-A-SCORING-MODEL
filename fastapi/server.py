148d2a600a84d55c67148d3e747fad6afcaacf1b
Samir Hinojosa <samirhinojosa@gmail.com>
2022-01-14 02:01:30 -0500

    adding fastapi and streamlit

---
+++ fastapi/server.py
@@ -0,0 +1,15 @@
+import io
+
+from fastapi import FastAPI, File
+
+app = FastAPI(
+    title="DeepLabV3 image segmentation",
+    description="""Obtain semantic segmentation maps of the image in input via DeepLabV3 implemented in PyTorch.
+                           Visit this URL at port 8501 for the streamlit interface.""",
+    version="0.1.0",
+)
+
+
+@app.get("/")
+async def root():
+    return {"message": "Hello World"}
