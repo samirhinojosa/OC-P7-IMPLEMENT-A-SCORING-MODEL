fff6b276470d224ceb83c544d7900624ba227986
Samir Hinojosa <samirhinojosa@gmail.com>
2022-01-24 16:52:35 +0100

    models execution

--- streamlit/ui.py
+++ streamlit/ui.py
@@ -1,3 +1,14 @@
 import streamlit as st
 
 st.title("Hola mundo")
+
+
+
+import requests
+import pandas as pd
+
+# http://127.0.01:5000/ is from the flask api
+response = requests.get("http://localhost:8008/api/customers")
+#print(response.json())
+data_table1 = pd.DataFrame(response.json())
+st.write(data_table1)
