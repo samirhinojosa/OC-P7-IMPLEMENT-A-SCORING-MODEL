653ba89bd6b69700e69a43c6e55fadfb0895c6c8
Samir Hinojosa <samirhinojosa@gmail.com>
2022-01-26 10:39:16 +0100

    frontend

--- streamlit/main.py
+++ streamlit/main.py
@@ -1,125 +1,149 @@
 import streamlit as st
+from PIL import Image
 import requests
 
 
-st.set_page_config(page_title="Credit Score Service", page_icon="🤖")
-st.title("Welcome to Credit Score Service!")
+# Loading images to the website
+icon = Image.open("images/favicon.ico")
+image = Image.open("images/home-credit.png")
 
 
+# General settings
+st.set_page_config(
+    page_title="Home credit Default Risk",
+    page_icon=icon,
+    layout="wide",
+    initial_sidebar_state="expanded",
+    menu_items={
+        "Report a bug" : None,
+        "Get help" : "https://github.com/samirhinojosa/OC-P7-implement-a-scoring-model",
+        "About" : 
+        '''
+        Made with ❤️ by [@samirhinojosa](https://www.samirhinojosa.com/) in OpenClassRooms Data Scientist Training.
+
+        For more information visit the following [link](https://github.com/samirhinojosa/OC-P7-implement-a-scoring-model).
+        '''
+    }
+)
+
+padding = 0
+st.markdown(f""" <style>
+    .reportview-container .main .block-container{{
+        padding-top: {padding}rem;
+        padding-right: {padding}rem;
+        padding-left: {padding}rem;
+        padding-bottom: {padding}rem;
+    }} </style> """, unsafe_allow_html=True
+)
+
+
+# Page information
+st.title("Home Credit - Default Risk")
+# st.header("This is the header")
+# st.subheader("This is the subheader")
+# st.text("Esto es una prueba.")
+# st.caption("This is a string that explains something above.")
+# st.markdown('Streamlit is **_really_ cool**.')
+# st.text("-------------FLAG-------------")
+
+# Session for the API
 def fetch(session, url):
+
     try:
         result = session.get(url)
         return result.json()
     except Exception:
         return {}
 
-
 session = requests.Session()
 
-
-def main():
-
+@st.cache
+def customer():
+    # Getting customers Id
     data = fetch(session, f"http://fastapi:8008/api/customers")
     if data:
-        st.dataframe(data)
+        return data["customersId"]
     else:
-        st.error("Error")
+        return "Error"
+
+def customer_details(id):
+    # Getting customer's details
+    data = fetch(session, f"http://fastapi:8008/api/customers/{id}")
+    if data:
+        return data
+    else:
+        return "Error"
 
 
-if __name__ == '__main__':
-    main()
 
-# def main():
-#     st.set_page_config(page_title="Example App", page_icon="🤖")
-#     st.title("Get Image by Id")
-#     session = requests.Session()
-#     with st.form("my_form"):
-#         index = st.number_input("ID", min_value=0, max_value=100, key="index")
+# Sidebar section
+st.sidebar.image(image)
+msg_info = st.sidebar.info("Select a customer to **predict** whether he will **pay the loan**")
+st.sidebar.markdown("🔎 **Customer selection**")
+customer_selection_form = st.sidebar.form(key="customer_selection")
+customer_id = customer_selection_form.selectbox(
+    "Customer ID", customer()
+)
+result = customer_selection_form.form_submit_button(label="Results")
 
-#         submitted = st.form_submit_button("Submit")
+if result:
+    
+    data = customer_details(customer_id)
+    customer_container = st.container()
 
-#         if submitted:
-#             st.write("Result")
-#             data = fetch(session, f"https://picsum.photos/id/{index}/info")
-#             if data:
-#                 st.image(data['download_url'], caption=f"Author: {data['author']}")
-#             else:
-#                 st.error("Error")
+    with customer_container:
+
+        st.subheader("Client's information")
+
+        col1_cc, col2_cc, col3_cc, col4_cc = customer_container.columns([2, 1, 1, 1])
+
+        with col1_cc:
+            import numpy as np
+            data2 = np.random.randn(10, 1)
+            col1_cc.line_chart(data2)
+        
+        with col2_cc:
+            st.markdown("**Customer id:**")
+            st.caption(data["customerId"])
+            st.markdown("**Children:**")
+            st.caption(data["children"])
+            st.markdown("**Years employed:**")
+            st.caption(data["yearsEmployed"])
+
+        with col3_cc:
+            st.markdown("**Gender:**")
+            st.caption(data["gender"])
+            st.markdown("**Own realty:**")
+            st.caption(data["ownRealty"])
+            st.markdown("**Total income:**")
+            st.caption(data["totalIncome"])
+
+        with col4_cc:
+            st.markdown("**Age:**")
+            st.caption(data["age"])
+            st.markdown("**Own Care:**")
+            st.caption(data["ownCar"])
+            st.markdown("**Current credit:**")
+            st.caption(data["credit"])
 
 
+
+
+
+
+
+# st.line_chart({"data": [1, 5, 2, 6, 2, 1]})
+
+# expander = st.expander("See explanation", expanded=True)
+
+# with expander:
+#      st.write("""
+#          The chart above shows some numbers I picked for you.
+#          I rolled actual dice for these, so they're *guaranteed* to
+#          be random.
+#      """)
+#      st.image("https://static.streamlit.io/examples/dice.jpg")
+
+#def main():
 # if __name__ == '__main__':
 #     main()
-
-
-
-
-
-
-
-
-#response = requests.get("http://localhost:8008/api/customers")
-#session = requests.Session()
-#data = fetch(session, f"http://localhost:8008/api/customers")
-#if data:
- #   print(response.json())
-    #data_table1 = pd.DataFrame(data.json())
-    #st.write(data_table1)
-#else:
- #    st.error("Error XX")
-
-# json = requests.get("http://fastapi:8008/api/customers").json()
-
-# st.dataframe(json)
-
-
-
-
-#import streamlit as st
-#import requests
-#import pandas as pd
-
-
-# def fetch(session, url):
-#     try:
-#         result = session.get(url)
-#         return result.json()
-#     except Exception:
-#         return {}
-
-
-# st.write("Here's our first attempt at using data to create a table:")
-# st.write(pd.DataFrame({
-#     'first column': [1, 2, 3, 4],
-#     'second column': [10, 20, 30, 40]
-# }))
-
-
-# df = pd.DataFrame({
-#     'first column': [1, 2, 3, 4],
-#     'second column': [10, 20, 30, 40]
-#     })
-
-# option = st.selectbox(
-#     'Which number do you like best?',
-#      df['first column'])
-
-# 'You selected: ', option
-
-#
-
-
-
-
-
-
-
-# import requests
-# import pandas as pd
-
-# # http://127.0.01:5000/ is from the flask api
-# response = requests.get("http://localhost:8008/api/customers")
-# #print(response.json())
-# data_table1 = pd.DataFrame(response.json())
-# st.write(data_table1)
-
-
