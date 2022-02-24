c2c3b468ff208f18375d631440c236ab73f7dc21
Samir Hinojosa <samirhinojosa@gmail.com>
2022-02-24 19:04:36 +0100

    final work

---
+++ supports/P7_HINOJOSA_Samir_v1/P7_04_notebook_functions.py
@@ -0,0 +1,284 @@
+# General
+import io
+import gc
+import timeit
+import pandas as pd
+import numpy as np
+from math import prod
+
+
+# Visualizations
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+
+def memory_usage(df):
+    """
+    Method used to calculate the used of memory based on a specific DataFrame.
+
+    Parameters:
+    -----------------
+        df (pandas.DataFrame): Dataset to analyze
+        
+    Returns:
+    -----------------
+        memory_usage (string) : The dataset's size on memory.
+    """
+    
+    # Calculating the memory usage based on dataframe.info()
+    buf = io.StringIO()
+    df.info(buf=buf)
+    memory_usage = buf.getvalue().split("\n")[-2]
+    
+    return (memory_usage)
+
+
+def memory_optimization(df):
+    """
+    Method used to optimize the memory usage.
+
+    Parameters:
+    -----------------
+        df (pandas.DataFrame): Dataset to analyze
+        
+    Returns:
+    -----------------
+        df (pandas.DataFrame): Dataset optimized
+    """ 
+    
+    for col in df.columns:
+        if df[col].dtype == "int64" and df[col].nunique() == 2:
+            df[col] = df[col].astype("int8")
+            
+    for col in df.columns:
+        if df[col].dtype == "float64" and df[col].min() >= -2147483648 and df[col].max() <= 2147483648:
+            df[col] = df[col].astype("float32")
+            
+    return df
+
+
+def df_analysis(df, name_df, *args, **kwargs):
+    """
+    Method used to analyze on the DataFrame.
+
+    Parameters:
+    -----------------
+        df (pandas.DataFrame): Dataset to analyze
+        name_df (str): Dataset name
+
+        *args, **kwargs:
+        -----------------
+            columns (list): Dataframe keys in list format
+            flag (str): Flag to show complete information about
+                        the dataset to analyse "complete" shows
+                        all information about the dataset
+                        
+                analysis_type (str) : the analysis type are 
+                                      ["complete", "summarized", "header"]
+                                      By defaut "complete"
+
+    Returns:
+    -----------------
+        None.
+        Print the analysis on the Dataset.
+    """
+
+    # Getting the variables
+    columns = kwargs.get("columns", None)
+
+    # Getting the variables
+    # the analysis type are ["complete", "summarized", "header"]
+    # By defaut "complete"
+    analysis_type = kwargs.get("analysis_type", None)
+
+    columns_reduced = [
+        "name", "type", "records", "unique"
+    ]
+
+    columns_complete = columns_reduced + ["# NaN", "% NaN", "mean",
+                                          "min", "25%", "50%",
+                                          "75%", "max", "std"]
+
+    # Identifying the columns name length
+    columns_name_length = []
+    for col in df.columns:
+        columns_name_length.append(len(col))
+
+    # Calculating the memory usage based on dataframe.info()
+    memory = memory_usage(df)
+
+    if df.empty:
+        print("The", name_df, "dataset is empty. Please verify the file.")
+    else:
+        empty_cols = [col for col in df.columns
+                      if df[col].isna().all()]  # identifying empty columns
+        df_rows_duplicates = df[df.duplicated()]  # identifying duplicates rows
+        
+
+        # Creating a dataset based on Type object and records by columns
+        type_cols = df.dtypes.apply(lambda x: x.name).to_dict()
+        df_resume = pd.DataFrame(list(type_cols.items()),
+                                 columns=["name", "type"])
+        df_resume["records"] = list(df.count())
+        df_resume["unique"] = list(df.nunique())
+        df_resume["# NaN"] = list(df.isnull().sum())
+        df_resume["% NaN"] = list(((df.isnull().sum()
+                                    / len(df.index))*100).round(2))
+        
+        print("\nAnalysis Header of", name_df, "dataset")
+        print(80*"-")
+
+        print("- Dataset shape:\t\t\t", df.shape[0], "rows and",
+              df.shape[1], "columns")
+        print("- Total of NaN values:\t\t\t", df.isna().sum().sum())
+        print("- Percentage of NaN:\t\t\t",
+              round((df.isna().sum().sum()/prod(df.shape))
+                    * 100, 2), "%")
+        print("- Total of infinite values:\t\t", np.isinf(df).values.sum())
+        print("- Percentage of infinite values:\t",
+              round((np.isinf(df).values.sum()/prod(df.shape))
+                    * 100, 2), "%")
+        print("- Total of full duplicates rows:\t",
+              df_rows_duplicates.shape[0])
+
+        if df.dropna(axis="rows", how="all").shape[0] < df.shape[0]:
+            print("- Total of empty rows:\t\t\t",
+                  df.shape[0] - df.dropna(axis="rows", how="all"))
+        else:
+            print("- Total of empty rows:\t\t\t", "0")
+
+        print("- Total of empty columns:\t\t", len(empty_cols))
+
+        if len(empty_cols) == 1 or len(empty_cols) >= 1:
+            print("\t+ The empty column is:\t\t", empty_cols)
+        else:
+            None
+
+        print("- Unique indexes:\t\t\t", df.index.is_unique)
+        print("- Memory usage:\t\t\t\t",
+              memory.split("memory usage: ")[1])
+
+        if columns is not None:
+            string_present = "present multiple times in the dataframe."
+            string_used = "be used as a primary key."
+            string_error = "Column does not exist"
+
+            try:
+
+                if df.size == df.drop_duplicates(columns).size:
+                    print("\n- The key(s):\t", columns, "is not",
+                          string_present, "\n\t\t It CAN", string_used)
+                else:
+                    print("\n- The key(s):\t", columns, "is", string_present,
+                          "\n\t\t It CANNOT", string_used)
+            except Exception:
+                print("\n- The key(s):\t\033[31m", string_error,
+                      "\033[0m", sep="")
+
+        # Enabling the visualizacion of
+        # all columns, rows, cell and floar format
+        pd.set_option("display.max_rows", None)  # all rows
+        pd.set_option("display.max_columns", None)  # all cols
+        pd.set_option("display.max_colwidth", None)  # whole cell
+        pd.set_option("display.float_format",
+                      lambda x: "%.5f" % x)  # full floating number
+
+        if analysis_type == "complete" or analysis_type is None:
+            df_resume["unique"] = list(df.nunique())
+
+            # Adding describing columns
+            if (df.select_dtypes(["int64"]).shape[1] > 0 or
+                    df.select_dtypes(["float64"]).shape[1] > 0):
+
+                df_desc = pd.DataFrame(df.describe().T).reset_index()
+                df_desc = df_desc.rename(columns={"index": "name"})
+                df_resume = df_resume.merge(right=df_desc[["name", "mean",
+                                                           "min", "25%",
+                                                           "50%", "75%",
+                                                           "max", "std"]],
+                                            on="name", how="left")
+                df_resume = df_resume[columns_complete]
+
+                if max(columns_name_length) > 18:
+                    n = 132
+                else:
+                    n = 120
+            else:
+                df_resume = df_resume[columns_reduced]
+                n = 70
+
+            print("\nDetailed analysis of", name_df, "dataset")
+            print(n*"-")
+
+            display(df_resume.sort_values("records", ascending=False))
+
+            if (df.select_dtypes(["int64"]).shape[1] > 0 or
+                    df.select_dtypes(["float64"]).shape[1] > 0):
+                del [[df_resume, df_desc]]
+            else:
+                del [[df_resume]]
+
+            gc.collect()
+            df_resume, df_desc = (pd.DataFrame() for i in range(2))
+
+        elif analysis_type == "header":
+
+            df_resume = pd.DataFrame()
+            
+            del df_resume, df
+            gc.collect()
+
+        pd.reset_option("display.max_rows")  # reset max of showing rows
+        pd.reset_option("display.max_columns")  # reset max of showing cols
+        pd.reset_option("display.max_colwidth")  # reset width of showing cols
+        pd.reset_option("display.float_format")  # reset float format in cell
+        
+
+def barplot_and_pie(df, title, subtitle_keyword):
+    """
+    Method used to plot a bar and pie graph.
+
+    Parameters:
+    -----------------
+        df (pandas.DataFrame): Column dataset to analyze
+        title (str): Graph name
+        subtitle_keyword (str); Subtitle graph name
+
+    Returns:
+    -----------------
+        None.
+        PLot the graphs.
+    """
+
+    # Getting the data to plot them
+    data = df.sort_values(ascending=False).value_counts().values.tolist()
+    labels = df.sort_values(ascending=False).value_counts().index.tolist()
+
+    if len(data) > 10:
+        figsize = [25, 10]
+    else:
+        figsize = [9, 5]
+
+    # defining the color palette
+    colors = sns.color_palette()
+
+    # Setting up the fig
+    fig, (ax1, ax2) = plt.subplots(nrows=1, ncols=2, figsize=figsize)
+    fig.suptitle(title, size=25)
+
+    # Setting up the pieplot
+    ax1.set_title(subtitle_keyword + " by percentage (%)", size=14)
+    ax1.pie(x=data, labels=labels, colors=colors, autopct='%1.1f%%')
+
+    # Setting up the barplot
+    ax2.set_title(subtitle_keyword + " by quantity (#)", size=14)
+    plot = sns.barplot(x=labels, y=data, ax=ax2, palette=colors)
+    plot.set_xticklabels(labels=labels, rotation=70, size=12,
+                         horizontalalignment="right")
+
+    for index, d in enumerate(data):
+        plt.text(x=index, y=d+1, s=f"{d}", horizontalalignment="center",
+                 fontdict=dict(fontsize=10, color="gray"))
+
+    plt.tight_layout()
+    plt.show()
---
+++ supports/P7_HINOJOSA_Samir_v1/P7_05_dashboard.py
@@ -0,0 +1,304 @@
+import io
+import os
+import json 
+import gc
+import pandas as pd
+import numpy as np
+from datetime import date, timedelta
+from fastapi import FastAPI, File, HTTPException
+import lightgbm as lgb
+from lightgbm import LGBMClassifier
+import matplotlib.pyplot as plt
+import joblib
+
+
+app = FastAPI(
+    title="Home Credit Default Risk",
+    description="""Obtain information related to probability of a client defaulting on loan.""",
+    version="0.1.0",
+)
+
+
+def calculate_years(days):
+    """
+    Method used to calculate years based on date (today - quantity of days).
+
+    Parameters:
+    -----------------
+        days (int): Numbers of day to rest of today
+
+    Returns:
+    -----------------
+        years (int): Numbers of years
+    """
+
+    today = date.today()
+    initial_date = today - timedelta(abs(days))
+    years = today.year - initial_date.year - ((today.month, today.day) < (initial_date.month, initial_date.day))
+
+    return years
+
+
+########################################################
+# Columns to read on CSVs
+########################################################
+COLUMNS = [
+    "SK_ID_CURR", "AMT_INCOME_TOTAL", "CODE_GENDER", 
+    "DAYS_BIRTH", "DAYS_REGISTRATION", "DAYS_EMPLOYED", 
+    "AMT_CREDIT", "AMT_GOODS_PRICE", "EXT_SOURCE_2",
+    "EXT_SOURCE_3", 
+]
+
+
+########################################################
+# Reading the csv
+########################################################
+df_clients_to_predict = pd.read_csv("datasets/df_clients_to_predict_20220221.csv")
+df_current_clients = pd.read_csv("datasets/df_current_clients_20220221.csv")
+
+df_current_clients["AGE"] = df_current_clients["DAYS_BIRTH"].apply(lambda x: calculate_years(x))
+df_current_clients["YEARS_EMPLOYED"] = df_current_clients["DAYS_EMPLOYED"].apply(lambda x: calculate_years(x))
+df_current_clients["EXT_SOURCE_2"] = df_current_clients["EXT_SOURCE_2"].round(3)
+df_current_clients["EXT_SOURCE_3"] = df_current_clients["EXT_SOURCE_3"].round(3)
+
+df_current_clients_by_target_repaid = df_current_clients[df_current_clients["TARGET"] == 0]
+df_current_clients_by_target_not_repaid = df_current_clients[df_current_clients["TARGET"] == 1]
+
+
+
+@app.get("/api/clients")
+async def clients_id():
+    """ 
+    EndPoint to get all clients id
+    """
+    
+    clients_id = df_clients_to_predict["SK_ID_CURR"].tolist()
+
+    return {"clientsId": clients_id}
+
+
+@app.get("/api/clients/{id}")
+async def client_details(id: int):
+    """ 
+    EndPoint to get client's detail 
+    """ 
+
+    clients_id = df_clients_to_predict["SK_ID_CURR"].tolist()
+
+    if id not in clients_id:
+        raise HTTPException(status_code=404, detail="client's id not found")
+    else:
+        # Filtering by client's id
+        df_by_id = df_clients_to_predict[COLUMNS][df_clients_to_predict["SK_ID_CURR"] == id]
+        idx = df_clients_to_predict[df_clients_to_predict["SK_ID_CURR"]==id].index[0]
+
+        for col in df_by_id.columns:
+            globals()[col] = df_by_id.iloc[0, df_by_id.columns.get_loc(col)]
+        
+        client = {
+            "clientId" : int(SK_ID_CURR),
+            "gender" : "Man" if int(CODE_GENDER) == 0 else "Woman",
+            "age" : calculate_years(int(DAYS_BIRTH)),
+            "antiquity" : calculate_years(int(DAYS_REGISTRATION)),
+            "yearsEmployed" : calculate_years(int(DAYS_EMPLOYED)),
+            "goodsPrice" : float(AMT_GOODS_PRICE),
+            "credit" : float(AMT_CREDIT),
+            "anualIncome" : float(AMT_INCOME_TOTAL),
+            "source2" : float(EXT_SOURCE_2),
+            "source3" : float(EXT_SOURCE_3),
+            "shapPosition" : int(idx)
+        }
+
+    return client
+
+
+@app.get("/api/predictions/clients/{id}")
+async def predict(id: int):
+    """ 
+    EndPoint to get the probability honor/compliance of a client
+    """ 
+
+    clients_id = df_clients_to_predict["SK_ID_CURR"].tolist()
+
+    if id not in clients_id:
+        raise HTTPException(status_code=404, detail="client's id not found")
+    else:
+        # Loading the model
+        model = joblib.load("models/model_20220220.pkl")
+
+        threshold = 0.135
+
+        # Filtering by client's id
+        df_prediction_by_id = df_clients_to_predict[df_clients_to_predict["SK_ID_CURR"] == id]
+        df_prediction_by_id = df_prediction_by_id.drop(df_prediction_by_id.columns[[0, 1]], axis=1)
+
+        # Predicting
+        result_proba = model.predict_proba(df_prediction_by_id)
+        y_prob = result_proba[:, 1]
+
+        result = (y_prob >= threshold).astype(int)
+
+        if (int(result[0]) == 0):
+            result = "Yes"
+        else:
+            result = "No"    
+
+    return {
+        "repay" : result,
+        "probability0" : result_proba[0][0],
+        "probability1" : result_proba[0][1],
+        "threshold" : threshold
+    }
+
+
+@app.get("/api/predictions/clients/shap/{id}")
+async def client_shap_df(id: int):
+    """ 
+    EndPoint to return a df with all client's data
+    """ 
+    
+    clients_id = df_clients_to_predict["SK_ID_CURR"].tolist()
+
+    if id not in clients_id:
+        raise HTTPException(status_code=404, detail="client's id not found")
+    else:
+
+        # Filtering by client's id
+        idx = df_clients_to_predict[df_clients_to_predict["SK_ID_CURR"]==id].index[0]
+
+        client = df_clients_to_predict[df_clients_to_predict["SK_ID_CURR"] == id].drop(columns=["SK_ID_CURR", "AMT_INCOME_TOTAL"])
+        client = client.to_json(orient="records")
+
+    return client
+
+
+@app.get("/api/statistics/ages")
+async def statistical_age():
+    """ 
+    EndPoint to get some statistics - ages
+    """
+
+    ages_data_repaid = df_current_clients_by_target_repaid.groupby("AGE").size()
+    ages_data_repaid = pd.DataFrame(ages_data_repaid).reset_index()
+    ages_data_repaid.columns = ["AGE", "AMOUNT"]
+    ages_data_repaid = ages_data_repaid.set_index("AGE").to_dict()["AMOUNT"]
+
+    ages_data_not_repaid = df_current_clients_by_target_not_repaid.groupby("AGE").size()
+    ages_data_not_repaid = pd.DataFrame(ages_data_not_repaid).reset_index()
+    ages_data_not_repaid.columns = ["AGE", "AMOUNT"]
+    ages_data_not_repaid = ages_data_not_repaid.set_index("AGE").to_dict()["AMOUNT"]
+
+    return {"ages_repaid" : ages_data_repaid, "ages_not_repaid" : ages_data_not_repaid}
+
+
+@app.get("/api/statistics/yearsEmployed")
+async def statistical_years_employed():
+    """ 
+    EndPoint to get some statistics - years employed
+    """
+
+    years_employed_data_repaid = df_current_clients_by_target_repaid.groupby("YEARS_EMPLOYED").size()
+    years_employed_data_repaid = pd.DataFrame(years_employed_data_repaid).reset_index()
+    years_employed_data_repaid.columns = ["YEARS_EMPLOYED", "AMOUNT"]
+    years_employed_data_repaid = years_employed_data_repaid.set_index("YEARS_EMPLOYED").to_dict()["AMOUNT"]
+
+    years_employed_data_not_repaid = df_current_clients_by_target_not_repaid.groupby("YEARS_EMPLOYED").size()
+    years_employed_data_not_repaid = pd.DataFrame(years_employed_data_not_repaid).reset_index()
+    years_employed_data_not_repaid.columns = ["YEARS_EMPLOYED", "AMOUNT"]
+    years_employed_data_not_repaid = years_employed_data_not_repaid.set_index("YEARS_EMPLOYED").to_dict()["AMOUNT"]
+
+    return {
+        "years_employed_repaid" : years_employed_data_repaid,
+        "years_employed_not_repaid" : years_employed_data_not_repaid
+    }
+
+
+@app.get("/api/statistics/amtCredits")
+async def statistical_amt_credit():
+    """ 
+    EndPoint to get some statistics - AMT Credit
+    """
+
+    amt_credit_data_repaid = df_current_clients_by_target_repaid.groupby("AMT_CREDIT").size()
+    amt_credit_data_repaid = pd.DataFrame(amt_credit_data_repaid).reset_index()
+    amt_credit_data_repaid.columns = ["AMT_CREDIT", "AMOUNT"]
+    amt_credit_data_repaid = amt_credit_data_repaid.set_index("AMT_CREDIT").to_dict()["AMOUNT"]
+
+    amt_credit_data_not_repaid = df_current_clients_by_target_not_repaid.groupby("AMT_CREDIT").size()
+    amt_credit_data_not_repaid = pd.DataFrame(amt_credit_data_not_repaid).reset_index()
+    amt_credit_data_not_repaid.columns = ["AMT_CREDIT", "AMOUNT"]
+    amt_credit_data_not_repaid = amt_credit_data_not_repaid.set_index("AMT_CREDIT").to_dict()["AMOUNT"]
+
+    return {
+        "amt_credit_repaid" : amt_credit_data_repaid,
+        "amt_credit_not_repaid" : amt_credit_data_not_repaid
+    }
+
+
+@app.get("/api/statistics/amtIncomes")
+async def statistical_amt_income():
+    """ 
+    EndPoint to get some statistics - AMT Income
+    """
+
+    amt_income_data_repaid = df_current_clients_by_target_repaid.groupby("AMT_INCOME_TOTAL").size()
+    amt_income_data_repaid = pd.DataFrame(amt_income_data_repaid).reset_index()
+    amt_income_data_repaid.columns = ["AMT_INCOME", "AMOUNT"]
+    amt_income_data_repaid = amt_income_data_repaid.set_index("AMT_INCOME").to_dict()["AMOUNT"]
+
+    amt_income_data_not_repaid = df_current_clients_by_target_not_repaid.groupby("AMT_INCOME_TOTAL").size()
+    amt_income_data_not_repaid = pd.DataFrame(amt_income_data_not_repaid).reset_index()
+    amt_income_data_not_repaid.columns = ["AMT_INCOME", "AMOUNT"]
+    amt_income_data_not_repaid = amt_income_data_not_repaid.set_index("AMT_INCOME").to_dict()["AMOUNT"]
+
+    return {
+        "amt_income_repaid" : amt_income_data_repaid,
+        "amt_income_not_repaid" : amt_income_data_not_repaid
+    }
+
+@app.get("/api/statistics/extSource2")
+async def statistical_ext_source_2():
+    """ 
+    EndPoint to get some statistics - EXT SOURCE 2
+    """
+
+    ext_source_2_data_repaid = df_current_clients_by_target_repaid.groupby("EXT_SOURCE_2").size()
+    ext_source_2_data_repaid = pd.DataFrame(ext_source_2_data_repaid).reset_index()
+    ext_source_2_data_repaid.columns = ["EXT_SOURCE_2", "AMOUNT"]
+    ext_source_2_data_repaid = ext_source_2_data_repaid.set_index("EXT_SOURCE_2").to_dict()["AMOUNT"]
+
+    ext_source_2_data_not_repaid = df_current_clients_by_target_not_repaid.groupby("EXT_SOURCE_2").size()
+    ext_source_2_data_not_repaid = pd.DataFrame(ext_source_2_data_not_repaid).reset_index()
+    ext_source_2_data_not_repaid.columns = ["EXT_SOURCE_2", "AMOUNT"]
+    ext_source_2_data_not_repaid = ext_source_2_data_not_repaid.set_index("EXT_SOURCE_2").to_dict()["AMOUNT"]
+
+    return {
+        "ext_source_2_repaid" : ext_source_2_data_repaid,
+        "ext_source_2_not_repaid" : ext_source_2_data_not_repaid
+    }
+
+
+@app.get("/api/statistics/extSource3")
+async def statistical_ext_source_3():
+    """ 
+    EndPoint to get some statistics - EXT SOURCE 3
+    """
+
+    ext_source_3_data_repaid = df_current_clients_by_target_repaid.groupby("EXT_SOURCE_3").size()
+    ext_source_3_data_repaid = pd.DataFrame(ext_source_3_data_repaid).reset_index()
+    ext_source_3_data_repaid.columns = ["EXT_SOURCE_3", "AMOUNT"]
+    ext_source_3_data_repaid = ext_source_3_data_repaid.set_index("EXT_SOURCE_3").to_dict()["AMOUNT"]
+
+    ext_source_3_data_not_repaid = df_current_clients_by_target_not_repaid.groupby("EXT_SOURCE_3").size()
+    ext_source_3_data_not_repaid = pd.DataFrame(ext_source_3_data_not_repaid).reset_index()
+    ext_source_3_data_not_repaid.columns = ["EXT_SOURCE_3", "AMOUNT"]
+    ext_source_3_data_not_repaid = ext_source_3_data_not_repaid.set_index("EXT_SOURCE_3").to_dict()["AMOUNT"]
+
+    return {
+        "ext_source_3_repaid" : ext_source_3_data_repaid,
+        "ext_source_3_not_repaid" : ext_source_3_data_not_repaid
+    }
+
+
+
+    
